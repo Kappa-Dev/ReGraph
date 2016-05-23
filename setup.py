cb40f4c675cee02c2eadb92bdc6b93444b56cbea
Eugenia <yarutoua@gmail.com>
2016-05-23 16:49:41 +0200

    Added: setup to install the library

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Setup of regraph library."""
+
+from setuptools import setup
+setup(name='ReGraph',
+      version='0.1',
+      description='Graph rewriting tool',
+      author='Eugenia Oshurko',
+      license='MIT License',
+      packages=['regraph',
+                'regraph.library'
+                ],
+      zip_safe=False)
