25cf0e27b84be7e72cba067e580b79ceb9ca275a
Eugenia <yarutoua@gmail.com>
2016-06-03 13:29:09 +0200

    Primitives adapted to undirected graphs + new add/delete attrs

--- regraph/library/primitives.py
+++ regraph/library/primitives.py
@@ -1,4 +1,6 @@
 """Defines primitives for graph transformation."""
+import warnings
+from copy import deepcopy
 
 
 def cast_node(graph, node, new_type):
@@ -89,66 +91,140 @@ def merge_nodes(graph, nodes, method="union",
     if method == "union":
         attr_accumulator = {}
     elif method == "intersection":
-        attr_accumulator = graph.node[nodes[0]].attrs_
+        attr_accumulator = deepcopy(graph.node[nodes[0]].attrs_)
     else:
         raise ValueError("Merging method %s is not defined!" % method)
-    source_nodes = set()
-    target_nodes = set()
-    source_dict = {}
-    target_dict = {}
+
+    self_loop = False
+    self_loop_attrs = {}
+
+    if graph.is_directed():
+        source_nodes = set()
+        target_nodes = set()
+
+        source_dict = {}
+        target_dict = {}
+    else:
+        neighbors = set()
+        neighbors_dict = {}
+
     for node in nodes:
+
         attr_accumulator = merge_attributes(
             attr_accumulator, graph.node[node].attrs_, method)
 
-        in_edges = graph.in_edges(node)
-        out_edges = graph.out_edges(node)
+        if graph.is_directed():
+            in_edges = graph.in_edges(node)
+            out_edges = graph.out_edges(node)
 
-        source_nodes.update(
-            [n if n not in nodes else node_name
-             for n, _ in graph.in_edges(node)])
-        target_nodes.update(
-            [n if n not in nodes else node_name
-             for _, n in graph.out_edges(node)])
+            # manage self loops
+            for s, t in in_edges:
+                if s in nodes:
+                    self_loop = True
+                    if len(self_loop_attrs) == 0:
+                        self_loop_attrs = graph.edge[s][t]
+                    else:
+                        self_loop_attrs = merge_attributes(
+                            self_loop_attrs,
+                            graph.edge[s][t],
+                            edge_method)
 
-        for edge in in_edges:
-            if not edge[0] in source_dict.keys():
-                attrs = graph.edge[edge[0]][edge[1]]
-                source_dict.update({edge[0]: attrs})
-            else:
-                attrs = merge_attributes(
-                    source_dict[edge[0]],
-                    graph.edge[edge[0]][edge[1]],
-                    edge_method)
-                source_dict.update({edge[0]: attrs})
+            for s, t in out_edges:
+                if t in nodes:
+                    self_loop = True
+                    if len(self_loop_attrs) == 0:
+                        self_loop_attrs = graph.edge[s][t]
+                    else:
+                        self_loop_attrs = merge_attributes(
+                            self_loop_attrs,
+                            graph.edge[s][t],
+                            edge_method)
 
-        for edge in out_edges:
-            if not edge[1] in target_dict.keys():
-                attrs = graph.edge[edge[0]][edge[1]]
-                target_dict.update({edge[1]: attrs})
-            else:
-                attrs = merge_attributes(
-                    target_dict[edge[1]],
-                    graph.edge[edge[0]][edge[1]],
-                    edge_method)
-                target_dict.update({edge[1]: attrs})
+            source_nodes.update(
+                [n if n not in nodes else node_name
+                 for n, _ in in_edges])
+            target_nodes.update(
+                [n if n not in nodes else node_name
+                 for _, n in out_edges])
 
-        # ! Here usage of nx.DiGraph.remove_node causes
-        # error (maybe some bug in networkx)
-        # print(graph.nodes())
-        # print(graph.edges())
+            for edge in in_edges:
+                if not edge[0] in source_dict.keys():
+                    attrs = graph.edge[edge[0]][edge[1]]
+                    source_dict.update({edge[0]: attrs})
+                else:
+                    attrs = merge_attributes(
+                        source_dict[edge[0]],
+                        graph.edge[edge[0]][edge[1]],
+                        edge_method)
+                    source_dict.update({edge[0]: attrs})
+
+            for edge in out_edges:
+                if not edge[1] in target_dict.keys():
+                    attrs = graph.edge[edge[0]][edge[1]]
+                    target_dict.update({edge[1]: attrs})
+                else:
+                    attrs = merge_attributes(
+                        target_dict[edge[1]],
+                        graph.edge[edge[0]][edge[1]],
+                        edge_method)
+                    target_dict.update({edge[1]: attrs})
+        else:
+            for n in graph.neighbors(node):
+                if n in nodes:
+                    self_loop = True
+                    if len(self_loop_attrs) == 0:
+                        self_loop_attrs = graph.edge[n][node]
+                    else:
+                        self_loop_attrs = merge_attributes(
+                            self_loop_attrs,
+                            graph.edge[n][node],
+                            edge_method)
+
+            neighbors.update(
+                [n for n in graph.neighbors(node) if n not in nodes])
+            for n in graph.neighbors(node):
+                if n not in nodes:
+                    if n not in neighbors_dict.keys():
+                        attrs = graph.edge[n][node]
+                        neighbors_dict.update({n: attrs})
+                    else:
+                        attrs = merge_attributes(
+                            neighbors_dict[n],
+                            graph.edge[n][node],
+                            edge_method)
+                        neighbors_dict.update({n: attrs})
+
         graph.remove_node(node)
 
     graph.add_node(node_name, node_type, attr_accumulator)
-    graph.add_edges_from([(n, node_name) for n in source_nodes])
-    graph.add_edges_from([(node_name, n) for n in target_nodes])
 
-    # Attach accumulated attributes to edges
-    for node, attrs in source_dict.items():
-        if node not in nodes:
-            graph.edge[node][node_name] = attrs
-    for node, attrs in target_dict.items():
-        if node not in nodes:
-            graph.edge[node_name][node] = attrs
+    if graph.is_directed():
+        if self_loop:
+            graph.add_edges_from([(node_name, node_name)])
+            graph.edge[node_name][node_name] = self_loop_attrs
+
+        graph.add_edges_from([(n, node_name) for n in source_nodes])
+        graph.add_edges_from([(node_name, n) for n in target_nodes])
+
+        # Attach accumulated attributes to edges
+        for node, attrs in source_dict.items():
+            if node not in nodes:
+                graph.edge[node][node_name] = attrs
+        for node, attrs in target_dict.items():
+            if node not in nodes:
+                graph.edge[node_name][node] = attrs
+    else:
+        if self_loop:
+            graph.add_edges_from([(node_name, node_name)])
+            graph.set_edge(node_name, node_name, self_loop_attrs)
+
+        graph.add_edges_from([(n, node_name) for n in neighbors])
+
+        # Attach accumulated attributes to edges
+        for node, attrs in neighbors_dict.items():
+            if node not in nodes:
+                graph.set_edge(node, node_name, attrs)
+
     return node_name
 
 
@@ -168,19 +244,28 @@ def clone_node(graph, node, name=None):
             new_node = name
 
     graph.add_node(new_node, graph.node[node].type_,
-                   graph.node[node].attrs_)
+                   deepcopy(graph.node[node].attrs_))
 
     # Connect all the edges
-    graph.add_edges_from(
-        [(n, new_node) for n, _ in graph.in_edges(node)])
-    graph.add_edges_from(
-        [(new_node, n) for _, n in graph.out_edges(node)])
+    if graph.is_directed():
+        graph.add_edges_from(
+            [(n, new_node) for n, _ in graph.in_edges(node)])
+        graph.add_edges_from(
+            [(new_node, n) for _, n in graph.out_edges(node)])
 
-    # Copy the attributes of the edges
-    for s, t in graph.in_edges(node):
-        graph.edge[s][new_node] = graph.edge[s][t]
-    for s, t in graph.out_edges(node):
-        graph.edge[new_node][t] = graph.edge[s][t]
+        # Copy the attributes of the edges
+        for s, t in graph.in_edges(node):
+            graph.edge[s][new_node] = deepcopy(graph.edge[s][t])
+        for s, t in graph.out_edges(node):
+            graph.edge[new_node][t] = deepcopy(graph.edge[s][t])
+    else:
+        graph.add_edges_from(
+            [(n, new_node) for n in graph.neighbors(node)])
+
+        # Copy the attributes of the edges
+        for n in graph.neighbors(node):
+            graph.set_edge(new_node, n, deepcopy(graph.edge[n][node]))
+
     return new_node
 
 
@@ -231,5 +316,95 @@ def remove_edge(graph, source, target):
         graph.remove_edge(source, target)
     else:
         raise ValueError(
-            "Edge %s->%s does not exist!", (str(source), str(target)))
+            "Edge %s->%s does not exist!" % (str(source), str(target)))
     return
+
+
+def add_node_attrs(graph, node, attrs_dict):
+    if node not in graph.nodes():
+        raise ValueError("Node %s does not exist" % str(node))
+    else:
+        for key, value in attrs_dict.items():
+            if key not in graph.node[node].attrs_.keys():
+                graph.node[node].attrs_.update({key: value})
+            else:
+                if type(value) != set:
+                    value = set([value])
+                if type(graph.node[node].attrs_[key]) != set:
+                    graph.node[node].attrs_[key] =\
+                        set([graph.node[node].attrs_[key]])
+                graph.node[node].attrs_[key].update(value)
+        print(node, graph.node[node].attrs_)
+
+def remove_node_attrs(graph, node, attrs_dict):
+    if node not in graph.nodes():
+        raise ValueError("Node %s does not exist" % str(node))
+    else:
+        for key, value in attrs_dict.items():
+            if key not in graph.node[node].attrs_.keys():
+                warnings.warn(
+                    "Node %s does not have attribute '%s'" % (str(node), str(key)), RuntimeWarning)
+            else:
+                if type(value) != set:
+                    value = set([value])
+                if type(graph.node[node].attrs_[key]) != set:
+                    graph.node[node].attrs_[key] =\
+                        set([graph.node[node].attrs_[key]])
+                for el in value:
+                    if el in graph.node[node].attrs_[key]:
+                        graph.node[node].attrs_[key].remove(el)
+                    else:
+                        warnings.warn(
+                            "Node %s does not have attribute '%s' with value '%s'" %
+                            (str(node), str(key), str(el)), RuntimeWarning)
+        print(graph.node[node].attrs_)
+
+
+def add_edge_attrs(graph, node_1, node_2, attrs_dict):
+    if (node_1, node_2) not in graph.edges():
+        raise ValueError("Edge %s-%s does not exist" % (str(node_1), str(node_2)))
+    else:
+        for key, value in attrs_dict.items():
+            if key not in graph.edge[node_1][node_2].keys():
+                graph.edge[node_1][node_2].update({key: value})
+                if not graph.is_directed():
+                    graph.edge[node_2][node_1].update({key: value})
+            else:
+                if type(value) != set:
+                    value = set([value])
+                if type(graph.edge[node_1][node_2][key]) != set:
+                    graph.edge[node_1][node_2][key] =\
+                        set([graph.edge[node_1][node_2][key]])
+                    if not graph.is_directed():
+                        graph.edge[node_2][node_1][key] =\
+                            set([graph.edge[node_2][node_1][key]])
+                graph.edge[node_1][node_2][key].update(value)
+                if not graph.is_directed():
+                    graph.edge[node_2][node_1][key].update(value)
+
+
+def remove_edge_attrs(graph, node_1, node_2, attrs_dict):
+    if (node_1, node_2) not in graph.edges() and (node_2, node_1) not in graph.edges():
+        raise ValueError("Edge %s-%s does not exist" % (str(node_1), str(node_2)))
+    else:
+        for key, value in attrs_dict.items():
+            if key not in graph.edge[node_1][node_2].keys():
+                warnings.warn(
+                    "Edge %s-%s does not have attribute '%s'" %
+                    (str(node_1), str(node_2), str(key)), RuntimeWarning)
+            else:
+                if type(value) != set:
+                    value = set([value])
+                if type(graph.edge[node_1][node_2][key]) != set:
+                    graph.edge[node_1][node_2][key] =\
+                        set([graph.edge[node_1][node_2][key]])
+                    if not graph.is_directed():
+                        graph.edge[node_2][node_1][key] =\
+                            set([graph.edge[node_2][node_1][key]])
+                for el in value:
+                    if el in graph.edge[node_1][node_2][key]:
+                        graph.edge[node_1][node_2][key].remove(el)
+                    else:
+                        warnings.warn(
+                            "Edge %s-%s does not have attribute '%s' with value '%s'" %
+                            (str(node_1), str(node_2), str(key), str(el)), RuntimeWarning)
