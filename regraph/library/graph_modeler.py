45d204f8dbf7ca317bb6c1152f8a5ec91926f93f
Ismail Lahkim Bennani <lahkim.bennani.ismail@gmail.com>
2016-06-14 15:36:00 +0200

    Minor changes

--- regraph/library/graph_modeler.py
+++ regraph/library/graph_modeler.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import warnings
 
 from regraph.library.data_structures import (TypedGraph,
                                              TypedDiGraph,
@@ -16,17 +17,17 @@ class GraphModeler(object):
         """
         typing_graph = None
         homL.insert(0, None)
-        self.rw_chain = []
-        self.rw_names = []
+        self.graph_chain = []
+        self.graph_names = []
         self.changes = []
 
         for i in range(len(l)):
             directed = type(l[î]) == nx.DiGraph
             typing_graph = self.make_tygraph(l[i][0], typing_graph, hom[i], directed)
-            self.rw_chain[i] = Rewriter(typing_graph)
+            self.graph_chain[i] = typing_graph
 
             if i>len(names):
-                self.rw_names[i] = None
+                self.graph_names[i] = None
             else:
                 if names[i] in self.rw_names:
                     raise ValueError(
@@ -36,7 +37,7 @@ class GraphModeler(object):
                     raise ValueError(
                         "Graph names have to be Strings"
                     )
-                self.rw_names[i] = names[i]
+                self.graph_names[i] = names[i]
 
     def make_tygraph(self, G, T, hom, di):
         res = TypedDiGraph(T) if di else TypedGraph(T)
@@ -48,11 +49,11 @@ class GraphModeler(object):
         return res
 
     def get_by_id(self, n):
-        return self.rw_chain[n]
+        return self.graph_chain[n]
 
     def get_id_from_name(self, name):
-        for i in range(self.rw_names):
-            if self.rw_names[i] == name:
+        for i in range(self.graph_names):
+            if self.graph_names[i] == name:
                 return i
         raise ValueError(
             "Graph %s is not defined in the modeler"% name
@@ -60,29 +61,29 @@ class GraphModeler(object):
 
     def get_by_name(self, name):
         for i in range(self.rw_names):
-            if self.rw_names[i] == name:
-                return self.rw_chain[i]
+            if self.graph_names[i] == name:
+                return self.graph_chain[i]
         raise ValueError(
             "Graph %s is not defined in the modeler" % name
         )
 
-    def rewrite(self, n_i, left_h, right_h):
+    def rewrite(self, n_i, L_T, left_h, right_h):
         """ n_i : name or id
             left_h : P -> L
             right_h : P -> R
         """
         if type(n_i) == int:
-            rew = self.get_by_id(n_i)
+            graph = self.get_by_id(n_i)
             i = n_i
         elif type(n_i) == str:
-            rew = self.get_by_name(n_i)
+            graph = self.get_by_name(n_i)
             i = self.get_id_from_name(n_i)
         else:
             raise ValueError(
                 "Undefined identifier of type %s, was expecting int or str" %
                 type(n_i)
             )
-        changes[i] = rew.typed_rewriting(left_h, right_h)
+        changes[i] = Rewriter.typed_rewriting(graph, L_T, left_h, right_h)
 
     def propagate_from(self, n_i):
         """ n_i : name or id
@@ -96,18 +97,24 @@ class GraphModeler(object):
                 "Undefined identifier of type %s, was expecting int or str" %
                 type(n_i)
             )
-        if i >= len(self.rw_chain)-1:
+        if i >= len(self.graph_chain)-1:
             return
         else:
-            G = self.rw_chain[i+1].graph_
-            T = G.metamodel_
-            T-_Tprime, T-_T, junk = changes[i]
-            G-, G-_G, G-_T- = pullback(G.hom, T-_T)
-            Gprime, G-_Gprime, Gprime_Tprime = pullback_complement(G-_T-, T-_Tprime)
-            Gprime.metamodel_ = Gprime_Tprime.target_
-            Gprime.hom = Gprime_Tprime
-            changes[i+1] = G-_Gprime, G-_G, Gprime_Tprime
-            propagate_from(self, i+1)
+            if changes[i] != None:
+                G = self.graph_chain[i+1].graph_
+                T = G.metamodel_
+                T-_Tprime, T-_T = changes[i]
+                G-, G-_G, G-_T- = pullback(G.hom, T-_T)
+                Gprime, G-_Gprime, Gprime_Tprime = pullback_complement(G-_T-, T-_Tprime)
+                Gprime.metamodel_ = Gprime_Tprime.target_
+                Gprime.hom = Gprime_Tprime
+                changes[i+1] = G-_Gprime, G-_G, Gprime_Tprime
+                propagate_from(self, i+1)
+            else:
+                warnings.warn(
+                    "Nothing to propagate from here !", RuntimeWarning
+                )
+
 
     def propagate_all(self):
         for i in range(len(changes)-1, -1, -1):
@@ -116,5 +123,5 @@ class GraphModeler(object):
 
     def commit_changes(self):
         for i in range(len(changes)):
-            rw_chain[i] = Rewriter(changes[i][0].target_)
+            graph_chain[i] = Rewriter(changes[i][0].target_)
             changes[i] = None
--- regraph/library/rewriters.py
+++ regraph/library/rewriters.py
@@ -2,6 +2,7 @@
 import networkx as nx
 from networkx.algorithms import isomorphism
 import warnings
+from copy import deepcopy
 
 import itertools
 
@@ -16,6 +17,8 @@ class Rewriter:
     def __init__(self, graph):
         """Initialize Rewriter object with input graph."""
         self.graph_ = graph
+        self.fully_expanded_graph = deepcopy(graph)
+        self.h_exp = dict([(n,n) for n in graph.nodes()])
         self.parser_ = parser
         return
 
@@ -81,14 +84,14 @@ class Rewriter:
         elif len(origin) == 1 :
             warnings.warn(
                 "Node %s is atomic, can't expand it further" % str(origin[0]),
-                Warning
+                RuntimeWarning
             )
         else:
             raise ValueError(
                 "Node %s does not exist, can't expand it" % (str(n0))
             )
-
-    def rewriting(self, left_h, right_h):
+    @staticmethod
+    def rewriting(graph, L_G, left_h, right_h):
         """ left_h : P -> L
             right_h : P -> R
         """
@@ -97,11 +100,210 @@ class Rewriter:
                 "Can't rewrite, homomorphisms don't have the same preserved part"
             )
 
-        L_G_instances = self.find_matching(left_h.target_)
-        Gprime = self.graph_
-        for L_G in L_G_instances :
-            G-, P_G-, G-_G = pullback_complement(left_h, L_G)
-            Gprime, G-_Gprime, R_Gprime = pushout(P_G-, P_R)
-            Gprime.metamodel_ = self.graph_.metamodel_
-            Gprime.hom = Homomorphism.canonic_homomorphism (Gprime, self.metamodel_)
+        G-, P_G-, G-_G = pullback_complement(left_h, L_G)
+        Gprime, G-_Gprime, R_Gprime = pushout(P_G-, P_R)
+        Gprime.metamodel_ = graph.metamodel_
+        Gprime.hom = Homomorphism.canonic_homomorphism (Gprime, graph.metamodel_)
         return G-_Gprime, G-_G
+
+    @staticmethod
+    def find_matching(graph, pattern):
+        """Perform matching of the pattern graph."""
+        # NetworkX isomorphism lib crushes if the ids of nodes
+        # have different types (e.g ints and strings).
+        # For the sake of security we will temporarily make
+        # all the nodes ids to be int
+        labels_mapping = dict([(n, i + 1) for i, n in enumerate(graph.nodes())])
+        g = graph.relabel_nodes(labels_mapping)
+        matching_nodes = set()
+        # find all the nodes matching the nodes in pattern
+        for pattern_node in pattern.nodes():
+            for node in g.nodes():
+                if pattern.node[pattern_node].type_ == g.node[node].type_:
+                    if is_subdict(pattern.node[pattern_node].attrs_,
+                                  g.node[node].attrs_):
+                        matching_nodes.add(node)
+        reduced_graph = g.subgraph(matching_nodes)
+        instances = []
+        isomorphic_subgraphs = []
+        for sub_nodes in itertools.combinations(reduced_graph.nodes(),
+                                                len(pattern.nodes())):
+                subg = reduced_graph.subgraph(sub_nodes)
+                for edgeset in itertools.combinations(subg.edges(),
+                                                      len(pattern.edges())):
+                    if g.is_directed():
+                        edge_induced_graph = nx.DiGraph(list(edgeset))
+                        edge_induced_graph.add_nodes_from(
+                            [n for n in subg.nodes() if n not in edge_induced_graph.nodes()])
+                        GM = isomorphism.DiGraphMatcher(pattern, edge_induced_graph)
+                        for isom in GM.isomorphisms_iter():
+                            isomorphic_subgraphs.append((subg, isom))
+                    else:
+                        edge_induced_graph = nx.Graph(edgeset)
+                        edge_induced_graph.add_nodes_from(
+                            [n for n in subg.nodes() if n not in edge_induced_graph.nodes()])
+                        GM = isomorphism.GraphMatcher(pattern, edge_induced_graph)
+                        for isom in GM.isomorphisms_iter():
+                            isomorphic_subgraphs.append((subg, isom))
+        for subgraph, mapping in isomorphic_subgraphs:
+            # check node matches
+            # exclude subgraphs which nodes information does not
+            # correspond to pattern
+            for (pattern_node, node) in mapping.items():
+                if not pattern.node[pattern_node].type_ == subgraph.node[node].type_:
+                    break
+                if not is_subdict(pattern.node[pattern_node].attrs_, subgraph.node[node].attrs_):
+                    break
+            else:
+                # check edge attribute matched
+                for edge in pattern.edges():
+                    pattern_attrs = pattern.get_edge(edge[0], edge[1])
+                    target_attrs = subgraph.get_edge(mapping[edge[0]], mapping[edge[1]])
+                    if not is_subdict(pattern_attrs, target_attrs):
+                        break
+                else:
+                    instances.append(mapping)
+
+        # bring back original labeling
+        inverse_mapping = dict([(value, key) for key, value in labels_mapping.items()])
+        for instance in instances:
+            for key, value in instance.items():
+                instance[key] = inverse_mapping[value]
+        return instances
+
+    @staticmethod
+    def generate_rule(LHS, commands):
+        """Cast sequence of commands to homomorphisms."""
+        command_strings = [c for c in commands.splitlines() if len(c) > 0]
+        actions = []
+        for command in command_strings:
+            try:
+                parsed = parser.parseString(command).asDict()
+                actions.append(parsed)
+            except:
+                raise ValueError("Cannot parse command '%s'" % command)
+
+        P = LHS.copy()
+        RHS = LHS.copy()
+
+        pl_mapping = dict(zip(LHS.nodes(), LHS.nodes()))
+        pr_mapping = dict(zip(LHS.nodes(), LHS.nodes()))
+        # We modify P, RHS and respective mapping
+        # in the course of command parsing
+        for action in actions:
+            if action["keyword"] == "clone":
+                node_name = None
+                if "node_name" in action.keys():
+                    node_name = action["node_name"]
+                cloned_node = clone_node(P, action["node"], node_name)
+                pl_mapping[action["node"]] = action["node"]
+                pl_mapping.update({cloned_node: action["node"]})
+                clone_node(RHS, action["node"], node_name)
+            elif action["keyword"] == "merge":
+                method = None
+                node_name = None
+                edges_method = None
+                if "method" in action.keys():
+                    method = action["method"]
+                if "node_name" in action.keys():
+                    node_name = action["node_name"]
+                if "edges_method" in action.keys():
+                    edges_method = action["edges_method"]
+                merged_node = merge_nodes(
+                    RHS,
+                    action["nodes"],
+                    method,
+                    node_name,
+                    edges_method)
+                for node in action["nodes"]:
+                    pr_mapping.update({node: merged_node})
+            elif action["keyword"] == "add_node":
+                name = None
+                node_type = None
+                attrs = {}
+                if "node" in action.keys():
+                    name = action["node"]
+                if "type" in action.keys():
+                    node_type = action["type"]
+                if "attrubutes" in action.keys():
+                    attrs = action["attrubutes"]
+                add_node(RHS, node_type, name, attrs)
+            elif action["keyword"] == "delete_node":
+                remove_node(P, action["node"])
+                del pl_mapping[action["node"]]
+                del pr_mapping[action["node"]]
+                remove_node(RHS, action["node"])
+            elif action["keyword"] == "add_edge":
+                attrs = {}
+                if "attrubutes" in action.keys():
+                    attrs = action["attrubutes"]
+                add_edge(
+                    RHS,
+                    action["node_1"],
+                    action["node_2"],
+                    attrs)
+            elif action["keyword"] == "delete_edge":
+                remove_edge(
+                    P,
+                    action["node_1"],
+                    action["node_2"])
+                remove_edge(
+                    RHS,
+                    action["node_1"],
+                    action["node_2"])
+            elif action["keyword"] == "add_node_attrs":
+                add_node_attrs(
+                    RHS,
+                    action["node"],
+                    action["attributes"])
+            elif action["keyword"] == "add_edge_attrs":
+                add_edge_attrs(
+                    RHS,
+                    action["node_1"],
+                    action["node_2"],
+                    action["attributes"])
+            elif action["keyword"] == "delete_node_attrs":
+                remove_node_attrs(
+                    P,
+                    action["node"],
+                    action["attributes"])
+                remove_node_attrs(
+                    RHS,
+                    action["node"],
+                    action["attributes"])
+            elif action["keyword"] == "delete_edge_attrs":
+                remove_edge_attrs(
+                    P,
+                    action["node_1"],
+                    action["node_2"],
+                    action["attributes"])
+                remove_edge_attrs(
+                    RHS,
+                    action["node_1"],
+                    action["node_2"],
+                    action["attributes"])
+            elif action["keyword"] == "update_node_attrs":
+                remove_node_attrs(
+                    P,
+                    action["node"],
+                    P.node[action["node"]].attrs_)
+                update_node_attrs(
+                    RHS,
+                    action["node"],
+                    action["attributes"])
+            elif action["keyword"] == "update_edge_attrs":
+                remove_edge_attrs(
+                    P,
+                    action["node_1"],
+                    action["node_2"],
+                    P.edge[action["node_1"]][action["node_2"]])
+                update_edge_attrs(
+                    RHS,
+                    action["node_1"],
+                    action["node_2"],
+                    action["attributes"])
+            else:
+                raise ValueError("Unknown command")
+        h_p_lhs = Homomorphism(P, LHS, pl_mapping)
+        h_p_rhs = Homomorphism(P, RHS, pr_mapping)
+        return (h_p_lhs, h_p_rhs)
