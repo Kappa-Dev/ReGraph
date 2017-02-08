1528fcd09d7e37986beaf5ac360f8041f8019e67
eugenia <yarutoua@gmail.com>
2017-02-08 11:55:46 +0100

    Category operations are modified to preserve original naming

--- regraph/library/category_op.py
+++ regraph/library/category_op.py
@@ -3,7 +3,7 @@
 from regraph.library.data_structures import (TypedGraph,
                                              TypedDiGraph,
                                              Homomorphism)
-from regraph.library.utils import keys_by_value, merge_attributes
+from regraph.library.utils import keys_by_value, merge_attributes, dict_sub
 
 
 def pullback(h1, h2):
@@ -95,38 +95,80 @@ def pushout(h1, h2):
     f = h1.mapping_
     g = h2.mapping_
 
+    # add nodes to the graph
     for n in C.nodes():
-        D.add_node(n,
-                   C.node[n].type_,
-                   C.node[n].attrs_)
-        hom2[n] = n
-
-    for n in B.nodes():
-        pred_n = keys_by_value(f, n)
-        if len(pred_n) == 0:
+        a_keys = keys_by_value(g, n)
+        # addition of new nodes
+        if len(a_keys) == 0:
             new_name = n
             i = 1
             while new_name in D.nodes():
-                new_name = str(n) + str(i)
+                new_name = str(n) + "_" + str(i)
                 i += 1
             D.add_node(new_name,
-                       B.node[n].type_,
-                       B.node[n].attrs_)
-            hom1[n] = new_name
+                       C.node[n].type_,
+                       C.node[n].attrs_)
+            hom2[n] = n
+        # addition of preserved nodes
+        elif len(a_keys) == 1:
+            a_key = a_keys[0]
+            D.add_node(f[a_key],
+                       C.node[n].type_,
+                       B.node[f[a_key]].attrs_)
+            D.add_node_attrs(f[a_key],
+                             dict_sub(C.node[g[a_key]].attrs_, A.node[a_key].attrs_))
+            hom1[f[a_key]] = f[a_key]
+            hom2[g[a_key]] = f[a_key]
+        # addition of merged nodes
         else:
-            hom1[n] = hom2[g[pred_n[0]]]
-            D.add_node_attrs(hom1[n], B.node[n].attrs_)
+            merging_nodes = []
+            attrs = {}
+            for a_key in a_keys:
+                merging_nodes.append(f[a_key])
+                attrs = merge_attributes(attrs, B.node[f[a_key]].attrs_)
+            new_name = "_".join([str(node) for node in merging_nodes])
 
+            D.add_node(new_name, C.node[n].type_, attrs)
+            D.add_node_attrs(new_name, dict_sub(C.node[n].attrs_, attrs))
+            
+            for a_key in a_keys:
+                hom1[f[a_key]] = new_name
+                hom2[n] = new_name
+
+    for n in B.nodes():
+        if n not in f.values():
+            D.add_node(n, B.node[n].type_, B.node[n].attrs_)
+            hom1[n] = n
+
+    # add edges to the graph
     for (n1, n2) in C.edges():
-        D.add_edge(n1, n2, C.get_edge(n1, n2))
-
-    for (n1, n2) in B.edges():
-        if (hom1[n1], hom1[n2]) in D.edges():
-            D.add_edge_attrs(hom1[n1], hom1[n2], B.get_edge(n1, n2))
+        a_keys_1 = keys_by_value(g, n1)
+        a_keys_2 = keys_by_value(g, n2)
+        if len(a_keys_1) == 0 or len(a_keys_2) == 0:
+            D.add_edge(hom2[n1], hom2[n2], C.get_edge(n1, n2))
         else:
+            for a_key_1 in a_keys_1:
+                for a_key_2 in a_keys_2:
+                    if A.is_directed():
+                        if (f[a_key_1], f[a_key_2]) in B.edges():
+                            D.add_edge(hom2[n1], hom2[n2], B.get_edge(f[a_key_1], f[a_key_2]))
+                            D.add_edge_attrs(hom2[n1],
+                                             hom2[n2],
+                                             dict_sub(C.get_edge(n1, n2), A.get_edge(a_key_1, a_key_2)))
+                    else:
+                        if (f[a_key_1], f[a_key_2]) in B.edges() or (f[a_key_2], f[a_key_1]) in B.edges():
+                            D.add_edge(hom2[n1], hom2[n2], B.get_edge(f[a_key_1], f[a_key_2]))
+                            D.add_edge_attrs(hom2[n1],
+                                             hom2[n2],
+                                             dict_sub(C.get_edge(n1, n2), A.get_edge(a_key_1, a_key_2)))
+    for (n1, n2) in B.edges():
+        a_keys_1 = keys_by_value(f, n1)
+        a_keys_2 = keys_by_value(f, n2)
+        if len(a_keys_1) == 0 or len(a_keys_2) == 0:
             D.add_edge(hom1[n1], hom1[n2], B.get_edge(n1, n2))
-
-    return (D, Homomorphism(B, D, hom1), Homomorphism(C, D, hom2))
+    return (D,
+            Homomorphism(B, D, hom1),
+            Homomorphism(C, D, hom2))
 
 def pullback_complement(h1, h2):
     """ Given h1 : A -> B; h2 : B -> D returns C, rh1, rh2
@@ -157,13 +199,14 @@ def pullback_complement(h1, h2):
     A_D = Homomorphism.compose(h2, h1)
 
     DmB = D.sub(B, h2)
-
+    
     for n in A.nodes():
-        C.add_node(n,
+        C.add_node(g[f[n]],
                    A.node[n].type_,
-                   A.node[n].attrs_)
-        hom1[n] = n
-        hom2[n] = g[f[n]]
+                   dict_sub(D.node[g[f[n]]].attrs_, B.node[f[n]].attrs_))
+        C.add_node_attrs(g[f[n]], A.node[n].attrs_)
+        hom1[n] = g[f[n]]
+        hom2[g[f[n]]] = g[f[n]]
 
     for n in DmB.nodes():
         is_in_A = False
@@ -182,45 +225,17 @@ def pullback_complement(h1, h2):
                            DmB.node[n].attrs_)
             hom2[n] = n
 
-    for n1 in C.nodes():
-        for n2 in C.nodes():
-            if n1 in A.nodes():
-                if n2 in A.nodes():
-                    if (n1, n2) in A.edges():
-                        C.add_edge(n1,
-                                   n2,
-                                   A.get_edge(n1, n2))
-                    pred_n1 = keys_by_value(hom1, n1)[0]
-                    pred_n2 = keys_by_value(hom1, n2)[0]
-                    n11 = f[n1]
-                    n21 = f[n2]
-                    if ((g[n11], g[n21]) in D.edges() and\
-                        (n11, n21) not in B.edges()):
-                        C.add_edge(n1,
-                                   n2,
-                                   D.get_edge(g[n11], g[n21]))
-                else:
-                    if (A_D.mapping_[n1], n2) in D.edges():
-                        pred_n2 = keys_by_value(g, n2)
-                        if len(pred_n2) == 0 or\
-                           (len(pred_n2)>0 and (pred_n2[0], f[n2]) not in B.edges()):
-                            C.add_edge(n1,
-                                       n2,
-                                       D.get_edge(A_D.mapping_[n1], n2))
-            else:
-                if n2 in A.nodes():
-                    if (n1, A_D.mapping_[n2]) in D.edges():
-                        pred_n1 = keys_by_value(g, n1)
-                        if len(pred_n1) == 0 or\
-                           (len(pred_n1)>0 and (pred_n1[0], f[n2]) not in B.edges()):
-                            C.add_edge(n1,
-                                       n2,
-                                       D.get_edge(n1, A_D.mapping_[n2]))
+    # Add edges from preserved part
+    for (n1, n2) in A.edges():
+        attrs = dict_sub(D.get_edge(g[f[n1]], g[f[n2]]), B.get_edge(f[n1], f[n2]))
+        C.add_edge(hom1[n1], hom1[n2], attrs)
+        C.add_edge_attrs(hom1[n1], hom1[n2], A.get_edge(n1, n2))
 
-                else:
-                    if (n1, n2) in D.edges():
-                        C.add_edge(n1,
-                                   n2,
-                                   D.get_edge(n1, n2))
+    # Add remaining edges from D
+    for (n1, n2) in D.edges():
+        b_key_1 = keys_by_value(g, n1) 
+        b_key_2 = keys_by_value(g, n2)
+        if len(b_key_1) == 0 or len(b_key_2) == 0:
+            C.add_edge(n1, n2, D.get_edge(n1, n2))
 
     return C, Homomorphism(A, C, hom1), Homomorphism(C, D, hom2)
