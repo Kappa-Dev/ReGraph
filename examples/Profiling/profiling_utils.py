525fe595f6df16feb6f5b3cfeb1f91708d258b7d
Eugenia Oshurko <yarutoua@gmail.com>
2018-05-17 16:19:42 +0200

    Fix: number of db hits + renamed to total_db_hits

--- examples/Profiling/profiling_utils.py
+++ examples/Profiling/profiling_utils.py
@@ -1,9 +1,10 @@
-def number_of_DbHits(profile, nb=0):
-    # print(profile[4])
-    nb += profile[4]
-    children = profile[3]
-    if len(children) == 0:
-        return nb
-    else:
-        for child in children:
-            return number_of_DbHits(child, nb)
+"""Utils for profiling Cypher queries."""
+
+
+def total_db_hits(profile):
+    """Compute the total number of db hits of a query profile."""
+    nb = 0
+    for child in profile.children:
+        nb += total_db_hits(child)
+    nb += profile.db_hits
+    return nb
