37ced155e6df6ec2b45f1aa0c5e93660772cff67
Florian Peron <peron.florian01@gmail.com>
2018-05-30 11:19:17 +0200

    Add access_graph method

--- regraph/neo4j/database.py
+++ regraph/neo4j/database.py
@@ -2,6 +2,8 @@
 
 from neo4j.v1 import GraphDatabase
 
+from regraph.neo4j.graph import Neo4jGraph
+
 
 class Neo4jDatabase(object):
     """Class implementing neo4j graph db driver."""
@@ -15,3 +17,13 @@ class Neo4jDatabase(object):
     def close(self):
         """Close connection."""
         self._driver.close()
+
+    def access_graph(self, label):
+        """Add a graph to the database."""
+        if label in self._graphs:
+            g = Neo4jGraph(label, self)
+        else:
+            self._graphs.update(label)
+            g = Neo4jGraph(label, self, set_constraint=True)
+            # Create a node in the hierarchy...
+        return g
