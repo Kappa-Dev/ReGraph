38a7140044c2e9a542b02384c2cbe14315f0d00c
Florian Peron <peron.florian01@gmail.com>
2018-06-01 16:17:05 +0200

    pullback function + update notebook

--- regraph/neo4j/category_utils.py
+++ regraph/neo4j/category_utils.py
@@ -3,82 +3,56 @@
 import regraph.neo4j.cypher_utils as cypher
 
 
-def pullback(b, c, d, a=None):
+def pullback(b, c, d, a=None, inplace=False):
     """Find the pullback from b -> d <- c."""
     if a is None:
         a = "pb_" + "_".join([b, c, d])
     query = ""
+    carry_vars = set()
 
     # Match all the pair of nodes with the same image in d
     query +=\
         "OPTIONAL MATCH (n:{})-[:typing]->(:{})<-[:typing]-(m:{})\n\n".format(
-            b, d, c) +\
-        "WITH n, m, [] as new_props\n"
+            b, d, c)
 
-    # For each pair, collect all the properties
-    query += new_props_merge(["n", "m"], 'new_props')
+    # For each pair, collect all the merged properties
+    query += cypher.merge_properties(["n", "m"], 'new_props',
+                                     method='intersection',
+                                     carry_vars=carry_vars)
     # For each pair, create a new node
-    query += "\nCREATE (new_node_a:{})\n".format(a)  # a tester : provisoire
+    query += cypher.create_node(
+                        var_name="new_node_a",
+                        node_id="pb",
+                        node_id_var="id_var",
+                        label=a,
+                        carry_vars=carry_vars,
+                        ignore_naming=True)[0]
+    carry_vars.remove("id_var")
     query += "SET new_node_a += new_props\n"
     # Add the typing edges
     query += "WITH n, m, new_node_a\n"
     query += "MERGE (new_node_a)-[:typing]->(n)\n"
     query += "MERGE (new_node_a)-[:typing]->(m)\n\n"
 
+    carry_vars = set()
     query2 = ""
     query2 +=\
-        "MATCH (x:{})-[:typing]->(:{})-[:edge]-(:{})<-[:typing]-(y:{})\n".format(
+        "MATCH (x:{})-[:typing]->(:{})-[r1:edge]->(:{})<-[:typing]-(y:{}),\n".format(
             a, b, b, a) +\
-        "WHERE (x)-[:typing]->(:{})-[:edge]-(:{})<-[:typing]-(y)\n".format(
+        "(x)-[:typing]->(:{})-[r2:edge]->(:{})<-[:typing]-(y)\n".format(
             c, c)
-
+    # Collect all the merged properties of the edges r1 and r2
+    query2 += cypher.merge_properties(["r1", "r2"], 'new_props',
+                                      method='intersection',
+                                      carry_vars={'x', 'y'})
+    query2 += "MERGE (x)-[r:edge]->(y)\n"
+    query2 += "SET r += new_props"
 
     return query, query2
 
 
-def new_props_merge(node_var_list, new_props_var, carry_vars=None):
-    if carry_vars is None:
-        carry_vars = set(node_var_list)
-    query = ""
-    for node in node_var_list:
-        query +=\
-            "WITH {} + REDUCE(pairs = [], k in keys({}) | \n".format(
-                new_props_var, node) +\
-            "\tpairs + REDUCE(inner_pairs = [], v in {}[k] | \n".format(
-                node) +\
-            "\t\t inner_pairs + {{key: k, value: v}})) as {},  ".format(
-                new_props_var) +\
-            ", ".join(carry_vars) + "\n"
-    query +=\
-        "WITH apoc.map.groupByMulti({}, 'key') as {}, ".format(
-            new_props_var, new_props_var) +\
-        ", ".join(carry_vars) + "\n" +\
-        "WITH apoc.map.fromValues(REDUCE(pairs=[], k in keys({}) | \n".format(
-            new_props_var) +\
-        "\tpairs + [k, REDUCE(values=[], v in {}[k] | \n".format(
-            new_props_var) +\
-        "\t\tvalues + CASE WHEN v.value IN values THEN [] ELSE v.value END)])) as {}, ".format(
-            new_props_var) +\
-        ", ".join(carry_vars) + "\n"
 
-    return query
-
-
-print(pullback('graphB', 'graphC', 'graphD', a='graphA')[1])
-
-
-"""
-OPTIONAL MATCH (n:graphB)-[:typing]->(:graphD)<-[:typing]-(m:graphC)
-WITH [] as new_props, n, m
-WITH new_props + REDUCE(pairs = [], k in keys(n) | 
-      pairs + REDUCE(inner_pairs = [], v in n[k] | 
-         inner_pairs + {key: k, value: v})) as new_props, n, m
-WITH new_props + REDUCE(pairs = [], k in keys(m) | 
-    pairs + REDUCE(inner_pairs = [], v in m[k] | 
-         inner_pairs + {key: k, value: v})) as new_props,n,m
-WITH apoc.map.groupByMulti(new_props, 'key') as new_props, n, m
-WITH apoc.map.fromValues(REDUCE(pairs=[], k in keys(new_props) | 
-    pairs + [k, REDUCE(values=[], v in new_props[k] | 
-        values + CASE WHEN v.value IN values THEN [] ELSE v.value END)])) as new_props
-return new_props
-"""
+res = pullback('graphB', 'graphC', 'graphD', a='graphA')
+print(res[0])
+print('----------------')
+print(res[1])
