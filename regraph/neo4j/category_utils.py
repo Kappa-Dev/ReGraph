67763c3b7a293b8a9f8f734b6c40d686d91a5218
Florian Peron <peron.florian01@gmail.com>
2018-06-04 16:17:11 +0200

    Pushout: first version

--- regraph/neo4j/category_utils.py
+++ regraph/neo4j/category_utils.py
@@ -7,20 +7,20 @@ def pullback(b, c, d, a=None, inplace=False):
     """Find the pullback from b -> d <- c."""
     if a is None:
         a = "pb_" + "_".join([b, c, d])
-    query = ""
+    query1 = ""
     carry_vars = set()
 
     # Match all the pair of nodes with the same image in d
-    query +=\
+    query1 +=\
         "OPTIONAL MATCH (n:{})-[:typing]->(:{})<-[:typing]-(m:{})\n".format(
             b, d, c)
 
     # For each pair, collect all the merged properties
-    query += cypher.merge_properties(["n", "m"], 'new_props',
-                                     method='intersection',
-                                     carry_vars=carry_vars)
+    query1 += cypher.merge_properties(["n", "m"], 'new_props',
+                                      method='intersection',
+                                      carry_vars=carry_vars)
     # For each pair, create a new node
-    query += cypher.create_node(
+    query1 += cypher.create_node(
                         var_name="new_node_a",
                         node_id="pb",
                         node_id_var="id_var",
@@ -28,19 +28,19 @@ def pullback(b, c, d, a=None, inplace=False):
                         carry_vars=carry_vars,
                         ignore_naming=True)[0]
     carry_vars.remove("id_var")
-    query += "SET new_node_a += new_props\n"
+    query1 += "SET new_node_a += new_props\n"
     carry_vars.remove("new_props")
 
     # Add the typing edges
-    query += cypher.with_vars(carry_vars) + "\n"
-    query += cypher.create_edge('new_node_a', 'n', edge_label='typing') + "\n"
-    query += cypher.create_edge('new_node_a', 'm', edge_label='typing') + "\n"
+    query1 += cypher.with_vars(carry_vars) + "\n"
+    query1 += cypher.create_edge('new_node_a', 'n', edge_label='typing') + "\n"
+    query1 += cypher.create_edge('new_node_a', 'm', edge_label='typing') + "\n"
 
     # Add the graph edges
     carry_vars = set()
     query2 = ""
     query2 +=\
-        "MATCH (x:{})-[:typing]->(:{})-[r1:edge]->(:{})<-[:typing]-(y:{}),\n".format(
+        "OPTIONAL MATCH (x:{})-[:typing]->(:{})-[r1:edge]->(:{})<-[:typing]-(y:{}),\n".format(
             a, b, b, a) +\
         "(x)-[:typing]->(:{})-[r2:edge]->(:{})<-[:typing]-(y)\n".format(
             c, c)
@@ -51,11 +51,74 @@ def pullback(b, c, d, a=None, inplace=False):
     query2 += "MERGE (x)-[r:edge]->(y)\n"
     query2 += "SET r += new_props"
 
-    return query, query2
+    return query1, query2
 
 
 def pushout(a, b, c, d=None, inplace=False):
-    pass
+    """Find the pushout of the span b <- a -> c."""
+    if d is None:
+        d = "pb_" + "_".join([a, b, c])
+    carry_vars = set()
+
+    query1 =\
+        "OPTIONAL MATCH (n:{})\n".format(b) +\
+        "WHERE n IS NOT NULL\n" +\
+        "MERGE (n)-[:typing]->(m:{})\n".format(d) +\
+        "SET m += properties(n)" +\
+        "SET m.id = id(m)"
+
+    query2 =\
+        "//Add all the edges\n" +\
+        "OPTIONAL MATCH (x:{})<-[:typing]-(:{})-[r:edge]->(:{})-[:typing]->(y:{})\n".format(
+            d, b, b, d) +\
+        "WITH x, y, r WHERE r IS NOT NULL\n" +\
+        "MERGE (x)-[new_r:edge]->(y)\n" +\
+        "SET new_r += properties(r)"
+    query22 =\
+        "//We search for self loops\n" +\
+        "OPTIONAL MATCH (z:{})<-[:typing]-(y:{})-[r:edge]->(y)\n".format(
+            d, b, b) +\
+        "WITH z, r WHERE r IS NOT NULL\n" +\
+        "MERGE (x)-[new_r:edge]->(x)\n" +\
+        "SET new_r += properties(r)"
+
+    query3 =\
+        "OPTIONAL MATCH (n:{})\n".format(c) +\
+        "WHERE n IS NOT NULL\n" +\
+        "MERGE (n)-[:typing]->(m:{})\n".format(d) +\
+        "SET m += properties(n)"
+
+    query4 =\
+        "OPTIONAL MATCH (x:{})<-[:typing]-(:{})-[r:edge]->(:{})-[:typing]->(y:{})\n".format(
+            d, c, c, d) +\
+        "WITH x, y, r WHERE r IS NOT NULL\n" +\
+        "MERGE (x)-[new_r:edge]->(y)\n" +\
+        "SET new_r += properties(r)"
+    query44 =\
+        "//We search for self loops\n" +\
+        "OPTIONAL MATCH (z:{})<-[:typing]-(y:{})-[r:edge]->(y)\n".format(
+            d, c, c) +\
+        "WITH z, r WHERE r IS NOT NULL\n" +\
+        "MERGE (z)-[new_r:edge]->(z)\n" +\
+        "SET new_r += properties(r)"
+
+    query5 =\
+        "OPTIONAL MATCH (n:{})<-[:typing]-(:{})<-[:typing]-(:{})-[:typing]->(m:{})\n".format(
+            d, b, a, c)
+
+    query55 =\
+        "OPTIONAL MATCH (n:{})<-[:typing]-(:{})<-[:typing]-(:{})-[:typing]->(:{})-[:typing]->(m:{})\n".format(
+            d, b, a, c, d) +\
+        cypher.merging_query(original_vars=["n", "m"],
+                             merged_var="merged_node",
+                             merged_id="id",
+                             merged_id_var="new_id",
+                             node_label=d,
+                             edge_label=None,
+                             ignore_naming=True)[0] + "\n" +\
+        cypher.return_vars(['new_id'])
+
+    return query1, query2, query22, query3, query4, query44, query5, query55
 
 
 def pullback_complement(a, b, d, c=None, inplace=False):
@@ -64,3 +127,22 @@ def pullback_complement(a, b, d, c=None, inplace=False):
 
 def check_homomorphism(domain, codomain, total=True):
     pass
+
+
+res = pushout('graphA', 'graphB', 'graphC', 'graphD')
+print(res[0])
+print('//---------------')
+print(res[1])
+print('//---------------')
+print(res[2])
+print('//---------------')
+print(res[3])
+print('//---------------')
+print(res[4])
+print('//---------------')
+print(res[5])
+print('//---------------')
+print(res[6])
+print('//---------------')
+print(res[7])
+print('//---------------')
